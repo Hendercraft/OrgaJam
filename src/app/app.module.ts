18744db66a45a5799713d02bc9c09e508c0cf988
Guillaume <guiguiruff@gmail.com>
2022-05-15 17:21:20 +0200

    Creation of tthe servie for auth wtih firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
+import { AuthHeaderComponent } from './auth-header/auth-header.component';
+import { LoginComponent } from './login/login.component';
+import { NavShearchComponent } from './nav-shearch/nav-shearch.component';
+import { environment } from '../environments/environment';
+
+/*Firebase related import*/
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent
+    RegisterComponent,
+    AuthHeaderComponent,
+    LoginComponent,
+    NavShearchComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireDatabaseModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,14 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCtFPhAV4YXuepKARp3catNpYJJmFYZ-D4',
+    authDomain: 'orgajam-b3d51.firebaseapp.com',
+    databaseURL:
+      'https://orgajam-b3d51-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'orgajam-b3d51',
+    storageBucket: 'orgajam-b3d51.appspot.com',
+    messagingSenderId: '412331800725',
+    appId: '1:412331800725:web:d34d1f439310befde3c39c',
+    measurementId: 'G-J69M582XND',
+  },
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
