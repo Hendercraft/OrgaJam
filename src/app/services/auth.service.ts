ba60c9354cd59dd8a41daab16a079c1f07fd4141
CROUTS DE PAILLE Benjamin <bencdep@gmail.com>
2022-06-06 15:07:28 +0200

    Added AuthService

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import { User } from "./user";
+import {AngularFireAuth} from "@angular/fire/compat/auth";
+import {Router} from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  userData :any;
+  constructor(
+      private afAuth : AngularFireAuth,
+      private router : Router
+  ) {
+    this.afAuth.authState.subscribe((user) => {
+      if (user) {
+        this.userData =user;
+        localStorage.setItem('user', JSON.stringify(this.userData));
+        JSON.parse(localStorage.getItem('user')!);
+      }
+      else {
+        localStorage.setItem('user', 'null');
+        JSON.parse(localStorage.getItem('user')!);
+      }
+    });
+  }
 
-  constructor() { }
+  signUp(email:string,password:string){
+    return this.afAuth
+        .createUserWithEmailAndPassword(email,password)
+        .then((result)=> {
+          //this.sendVerificationMail();
+          //this.setUserData(result.user);
+            }
+        )
+        .catch((error)=> {
+          window.alert(error.message);
+        });
+  }
 }
+
