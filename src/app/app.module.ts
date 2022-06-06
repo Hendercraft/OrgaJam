89ec2c24fe59df128ed13a306280b34a6eac1aab
RUFF <guiguiruff@gmail.com>
2022-06-06 12:33:01 +0200

    new base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
+  firebase: {
+    projectId: 'orgajam-b3d51',
+    appId: '1:412331800725:web:d34d1f439310befde3c39c',
+    databaseURL: 'https://orgajam-b3d51-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'orgajam-b3d51.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyCtFPhAV4YXuepKARp3catNpYJJmFYZ-D4',
+    authDomain: 'orgajam-b3d51.firebaseapp.com',
+    messagingSenderId: '412331800725',
+    measurementId: 'G-J69M582XND',
+  },
   production: true
 };
